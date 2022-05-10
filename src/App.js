9d56885b9c1fa78af58ae0686ca443b0444d1414
andrewredman91 <andrewredman91@gmail.com>
2022-05-10 09:07:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [todos, setTodos] = useState([
+    {name: "Buy Shopping", priority:': low priority'},
+    {name: "Clean Bathroom", priority: ': high priority'},
+    {name: "Car's MOT", priority: ': low priority'},
+  ])
+
+const [newTodos, setNewTodos] = useState("") 
+const [newPriority, setNewPriority] = useState("") 
+
+const todosNodes = todos.map((todos, index) => { 
+  return(
+      <li key={index} >
+        
+      <span>{todos.name}</span>
+      <span>{todos.priority}</span>
+      </li> 
+  )
+})
+
+// const completeTodos = ((index) => {
+//   const copyTodos = [... todos]
+//   copyTodos[index].isCompleted = true
+//   setTodos(copyTodos)
+// })
+
+
+
+
+
+const handleTodosInput = (event) => {
+  setNewTodos(event.target.value)
 }
 
+const handlePriorityInput = (priority) => {
+  setNewPriority(priority.target.value)
+}
+const saveNewTodos = ((event) =>{
+  event.preventDefault()
+  const copyTodos = [... todos, {name: newTodos, priority: newPriority}]
+  setTodos(copyTodos)
+  setNewTodos("")
+})
+
+
+
+return (
+  <div className="App">
+  <h1>Todo List</h1>
+  <hr></hr>
+    <ul>
+        {todosNodes}
+    </ul>
+<form onSubmit={saveNewTodos}>
+      <label htmlFor="new-todos">Add a new todo</label>
+      <input id="new-todos" type="text" onChange={handleTodosInput} value={newTodos}/>
+      <input type="radio" name="something" value ="high" onChange={handlePriorityInput}/>
+      <label for="high">high priority</label>
+      <input type="radio"  value ="low" name="something" onChange={handlePriorityInput}/>
+      <label for="low">low priority</label>
+      <input type="submit" value="Save New Todo" />
+
+    </form>
+    </div>
+  );
+
+}
 export default App;
